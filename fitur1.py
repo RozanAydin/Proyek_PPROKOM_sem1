5369458b94714d090ae9ec25bf0461b325d30850
RozanAydin <rozanaydinz@gmail.com>
2024-11-21 09:07:01 +0700

    Update fitur1.py

--- fitur1.py
+++ fitur1.py
@@ -10,7 +10,7 @@ def tampilkan_menu():
     print("1. Tambah Materi Pembelajaaran ")
     print("2. Lihat Materi Pembelajaran")
     print("3. Revisi Materi Pembelajaran")
-    print("4. Keluar")
+    print("4. Kembali Ke Menu")
 
 def tambah_materi():
     judul = input("Masukkan judul materi: ")
@@ -48,26 +48,28 @@ def lihat_isi_materi():
 
 def revisi_materi():
     if materi_pembelajaran:
-        try:
-            nomor = int(input("Masukkan nomor materi yang ingin direvisi: ")) - 1
-            if 0 <= nomor < len(materi_pembelajaran):
-                materi = materi_pembelajaran[nomor]
-                print(f"\nJudul saat ini: {materi['judul']}")
-                print(f"Isi saat ini: {materi['isi']}")
-                
-                judul_baru = input("Masukkan judul baru (tekan Enter jika tidak ada perubahan): ")
-                isi_baru = input("Masukkan isi baru (tekan Enter jika tidak ada perubahan): ")
-                
-                if judul_baru:
-                    materi["judul"] = judul_baru
-                if isi_baru:
-                    materi["isi"] = isi_baru
-                
-                print("Materi berhasil diperbarui.\n")
-            else:
-                print("Nomor materi tidak valid.\n")
-        except ValueError:
-            print("Masukkan nomor yang valid.\n")
+        while True:
+            try:
+                nomor = int(input("Masukkan nomor materi yang ingin direvisi: ")) - 1
+                if 0 <= nomor < len(materi_pembelajaran):
+                    materi = materi_pembelajaran[nomor]
+                    print(f"\nJudul saat ini: {materi['judul']}")
+                    print(f"Isi saat ini: {materi['isi']}")
+                    
+                    judul_baru = input("Masukkan judul baru (tekan Enter jika tidak ada perubahan): ")
+                    isi_baru = input("Masukkan isi baru (tekan Enter jika tidak ada perubahan): ")
+                    
+                    if judul_baru:
+                        materi["judul"] = judul_baru
+                    if isi_baru:
+                        materi["isi"] = isi_baru
+                    
+                    print("Materi berhasil diperbarui.\n")
+                    break
+                else:
+                    print("Nomor materi tidak valid.\n")
+            except ValueError:
+                print("Masukkan nomor yang valid.\n")
     else:
         print("Belum ada materi yang tersedia.\n")
 
@@ -91,4 +93,5 @@ def menu_fitur1():
             bersihkan_layar()
             break
         else:
+            bersihkan_layar()
             print("Pilihan tidak valid. Silakan pilih opsi yang benar.\n")
